d64da699f5c9bef133ddb0c7da4fa0b80fb2c3e9
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-01-14 20:26:09 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//       $('.your-class').slick({
+//         setting-name: setting-value
+//       });
+//     });
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
