326f7460975650726d974103d90ec41dc588159b
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-02-06 01:35:00 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,12 +44,10 @@ const swiper = new Swiper('.swiper-contaner-second', {
 	   disableOnInteraction: false,
 	 },
 
- 	// If we need pagination
   	pagination: {
    		el: '.swiper-pagination',
   	},
 
-  	// Navigation arrows
   	navigation: {
     	nextEl: '.swiper-button-next',
     	prevEl: '.swiper-button-prev',
