5a947f94b41bc90a38330ee04f6562b659cc1d39
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-02-19 17:54:53 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	$('#pagepiling').pagepiling();
+	 $('.company__slider').slick({
+	 	dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+});
+
+ 
