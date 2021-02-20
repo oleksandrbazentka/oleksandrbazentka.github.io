3544c79d0fb7ff3ea52cfcfff9e46008fcf94919
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-02-20 02:56:17 +0200

    d

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-	$('#pagepiling').pagepiling();
+	AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 60, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 700, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+	$('#pagepiling').pagepiling({	
+	 menu: null,
+	  anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourPage', 'fivePage', 'sixthPage', 'seventhPage', 'eighthPage']
+});
+
 	 $('.company__slider').slick({
 	 	dots: false,
   infinite: false,
@@ -35,6 +62,16 @@ $(document).ready(function() {
     // instead of a settings object
   ]
 });
-});
 
- 
+	 $('.features__num').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+});
