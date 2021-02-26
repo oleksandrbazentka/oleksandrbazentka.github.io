d7b91c52535cf6f94e0404801d25e6d79f297c3d
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-02-26 23:25:06 +0200

    done

--- js/script.js
+++ js/script.js
@@ -15,34 +15,29 @@ const requestData = (id = 1) => {
 
 requestData();
 $mapLinks.forEach(el => {
-	// el.addEventListener('mouseenter', (e) => {
-	// 	let self = e.currentTarget;
-	// 	let selfClass = self.getAttribute('href');
-	// 	let color = self.dataset.color;
-	// 	let currentPolygon = self.querySelectorAll('polygon');
-	// 	let currentPath = self.querySelectorAll('path');
-	// 	if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
-	// 	if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
-	// });
-
-	// el.addEventListener('mouseleave', (e) => {
-	// 	let self = e.currentTarget;
-	// 	let selfClass = self.getAttribute('href');
-	// 	let currentPolygon = self.querySelectorAll('polygon');
-	// 	let currentPath = self.querySelectorAll('path');
-	// 	if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = ``);
-	// 	if (currentPath) currentPath.forEach(el => el.style.cssText = ``);
-	// });
-
-	el.addEventListener('click', (e) => {
-		e.preventDefault();
+	el.addEventListener('mouseenter', (e) => {
 		let self = e.currentTarget;
 		let selfClass = self.getAttribute('href');
 		let color = self.dataset.color;
 		let currentPolygon = self.querySelectorAll('polygon');
 		let currentPath = self.querySelectorAll('path');
-		if (currentPolygon) currentPolygon.forEach(el => el.style.fill = '#FFFFFF');
-		if (currentPath) currentPath.forEach(el => el.style.fill = '#FFFFFF');
+		if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+		if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+	});
+
+	el.addEventListener('mouseleave', (e) => {
+		let self = e.currentTarget;
+		let selfClass = self.getAttribute('href');
+		let currentPolygon = self.querySelectorAll('polygon');
+		let currentPath = self.querySelectorAll('path');
+		if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = ``);
+		if (currentPath) currentPath.forEach(el => el.style.cssText = ``);
+	});
+
+	el.addEventListener('click', (e) => {
+		e.preventDefault();
+		let self = e.currentTarget;
+		let selfClass = self.getAttribute('href');
 		let currentElement = document.querySelector(`.map-tab-link[href="${selfClass}"]`);
 		let id = parseInt(currentElement.dataset.id);
 		requestData(id);
