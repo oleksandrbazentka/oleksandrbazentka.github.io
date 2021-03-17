9d28192a078cb99c92642fbe3fccfd628e69786c
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-03-17 20:31:06 +0200

    done

--- js/swiper.js
+++
@@ -1,9 +0,0 @@
-var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-})
