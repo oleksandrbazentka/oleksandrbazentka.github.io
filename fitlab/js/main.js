1577d75af8d0e8e5956a8c36e74e9a7ee5db0134
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-04-05 17:49:07 +0300

    done

---
+++ fitlab/js/main.js
@@ -0,0 +1,11 @@
+// document.documentElement.classList.remove('no-js');
+
+var hamBtn = document.querySelector('.hamburger-btn');
+
+hamBtn.onclick = function() {
+	document.querySelector('.hamburger-menu').classList.toggle('dis-none');
+}
+
+$(document).ready(function() {
+	$("#phone").mask("(999) 999-9999");
+})
