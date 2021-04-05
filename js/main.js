f0651d74012f86af3fa596f41dff181b773bcd88
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-04-05 14:55:46 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,77 +1,19 @@
-$('.big-slider__items').slick({
-  dots: false,
-  appendArrows: false,
-  prevArrow: false,
-  nextArrow: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true
-});
+var ham = document.querySelector('.menu__list-ham');
+var btnHam = document.querySelector('.header__hamburger');
+var choose = document.querySelector('.main-profile__choose');
+var btnExit = document.querySelector('.main-profile__exit');
+var btnNo = document.querySelector('.choose__button_second');
 
-jQuery(function(f){
-	var element = f('#blfix');
-	f(window).scroll(function(){
-	    element['fade'+ (f(this).scrollTop() > 100 ? 'In': 'Out')](500);           
-	});
-}); 
+btnHam.onclick = function() {
+    ham.classList.toggle('dis-flex');
+} 
 
+btnExit.onclick = function() {
+	btnExit.style.display = "none";
+	choose.style.display = "flex";
+}
 
-$(function() {
-  $("#selectBackground ul li a").click(function(e) {
-    e.preventDefault();
-    $("#selectBackground ul li a").removeClass('active');
-    $(this).addClass('active');
-  })
-});
-
-$(function() {
-	$(".info__btn").click(function(e) {
-		$('.info__btn').removeClass('info__active');
-		$(this).addClass('info__active');
-	})
-
-	$("#info-1").click(function(e) {
-		$('.text-2').css('display', 'none');
-		$('.text-3').css('display', 'none');
-		$('.text-4').css('display', 'none');
-		$('.text-5').css('display', 'none');
-		$('.text-1').css('display', 'block');
-	})
-
-	$("#info-2").click(function(e) {
-		$('.text-1').css('display', 'none');
-		$('.text-3').css('display', 'none');
-		$('.text-4').css('display', 'none');
-		$('.text-5').css('display', 'none');
-		$('.text-2').css('display', 'block');
-	})
-
-	$("#info-3").click(function(e) {
-		$('.text-1').css('display', 'none');
-		$('.text-2').css('display', 'none');
-		$('.text-4').css('display', 'none');
-		$('.text-5').css('display', 'none');
-		$('.text-3').css('display', 'block');
-	})
-
-	$("#info-4").click(function(e) {
-		$('.text-1').css('display', 'none');
-		$('.text-2').css('display', 'none');
-		$('.text-3').css('display', 'none');
-		$('.text-5').css('display', 'none');
-		$('.text-4').css('display', 'block');
-	})
-
-	$("#info-5").click(function(e) {
-		$('.text-1').css('display', 'none');
-		$('.text-2').css('display', 'none');
-		$('.text-3').css('display', 'none');
-		$('.text-4').css('display', 'none');
-		$('.text-5').css('display', 'block');
-	})
-});
-
-
-
+btnNo.onclick = function() {
+	btnExit.style.display = "block";
+	choose.style.display = "none";
+}
