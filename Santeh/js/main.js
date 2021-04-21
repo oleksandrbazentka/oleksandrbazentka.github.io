9cdb907c915898cc1a071966866a284fa3883eef
oleksandrbazentka <62761065+oleksandrbazentka@users.noreply.github.com>
2021-04-21 20:32:09 +0300

    дан

--- Santeh/js/main.js
+++ Santeh/js/main.js
@@ -1,116 +1,29 @@
-let headerBtn = document.querySelector('.header__button');
-let footerBtn = document.querySelector('.footer__button');
-let closePopup = document.querySelector('.popup__close');
-let popupCheck = document.querySelector('.popup__checkbox');
-let submit = document.querySelector('.form__submit');
-let specBtn = document.querySelector('.install__btn');
-let closePopupSpec = document.querySelector('.close__spec');
-let checkboxSpec = document.querySelector('.popup__checkbox-spec');
-let submitSpec = document.querySelector('.submit-spec');
-let closeFooterPopup = document.querySelector('.popup__close-footer');
-let installBtn = document.querySelector('.install__link');
-let calculateBtn = document.querySelector('.calculate__btn'); 
-let closePrice = document.querySelector('.popup__close-price');
-let checkboxPrice = document.querySelector('.popup__checkbox-price');
-let submitPrice = document.querySelector('.submit-price');
-let headerBtnSm = document.querySelector('.header__button-sm');
-let footerBtnSm = document.querySelector('.footer__button-sm');
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+document.addEventListener('DOMContentLoaded', function() {
+    var modalButtons = document.querySelectorAll('.popup__button'),
+    closeButtons = document.querySelectorAll('.popup__close');
 
-headerBtn.onclick = function() {
-	document.querySelector('.popup__call-bg').style.display = 'flex';
-    document.querySelector('html').style.overflowY = 'hidden';
-}
+    modalButtons.forEach(function(item){
+      item.addEventListener('click', function(e) {
+         e.preventDefault();
+         var modalId = this.getAttribute('data-modal'),
+             modalElem = document.querySelector('.popup-bg[data-modal="' + modalId + '"]');
+         modalElem.classList.add('active');
+         document.querySelector('html').style.overflowY = 'hidden';
+      });
+   });
 
-headerBtnSm.onclick = function() {
-	document.querySelector('.popup__call-bg').style.display = 'flex';
-    document.querySelector('html').style.overflowY = 'hidden';
-}
-
-footerBtn.onclick = function() {
-	document.querySelector('.popup__cal-bg-footer').style.display = 'flex';
-    document.querySelector('html').style.overflowY = 'hidden';
-}
-
-footerBtnSm.onclick = function() {
-	document.querySelector('.popup__call-bg').style.display = 'flex';
-    document.querySelector('html').style.overflowY = 'hidden';
-}
-
-installBtn.onclick = function() {
-	document.querySelector('.popup__price-bg').style.display = 'flex';
-    document.querySelector('html').style.overflowY = 'hidden';
-}
-
-calculateBtn.onclick = function() {
-	document.querySelector('.popup__price-bg').style.display = 'flex';
-    document.querySelector('html').style.overflowY = 'hidden';
-}
-
-closePopup.onclick = function() {
-	document.querySelector('.popup__call-bg').style.display = 'none';
-    document.querySelector('html').style.overflowY = 'scroll';
-}
-
-closePopupSpec.onclick = function() {
-	document.querySelector('.popup__spec-bg').style.display = 'none';
-    document.querySelector('html').style.overflowY = 'scroll';
-}
-
-closeFooterPopup.onclick = function() {
-	document.querySelector('.popup__call-bg').style.display = 'none';
-    document.querySelector('html').style.overflowY = 'scroll';
-}
-
-closePrice.onclick = function() {
-	document.querySelector('.popup__price-bg').style.display = 'none';
-    document.querySelector('html').style.overflowY = 'scroll';
-}
-
-specBtn.onclick = function() {
-	document.querySelector('.popup__spec-bg').style.display = 'flex';
-    document.querySelector('html').style.overflowY = 'hidden';
-}
-
-submit.onclick = function() {
-	if (popupCheck.getAttribute("type") === "checked") {
-	return true;
-} else {
-	document.querySelector('.switch').style.boxShadow = '0px 0px 12px 9px rgba(253, 0, 0, 0.48)';
-}
-}
-
-submitSpec.onclick = function() {
-	if (checkboxSpec.getAttribute("type") === "checked") {
-	return true;
-} else {
-	document.querySelector('.switch-spec').style.boxShadow = '0px 0px 12px 9px rgba(253, 0, 0, 0.48)';
-}
-}
-
-submitPrice.onclick = function() {
-	if (checkboxPrice.getAttribute("type") === "checked") {
-	return true;
-} else {
-	document.querySelector('.switch-price').style.boxShadow = '0px 0px 12px 9px rgba(253, 0, 0, 0.48)';
-}
-}
-
-jQuery(document).ready(function(){
-    $('#tel').mask('+(000) 000 00 00', {placeholder: "Телефон"});
+   closeButtons.forEach(function(item){
+      item.addEventListener('click', function(e) {
+         var parentModal = this.closest('.popup-bg');
+         parentModal.classList.remove('active');
+         document.querySelector('html').style.overflowY = 'scroll';
+      });
+   });
 });
 
-$(document).ready(function() {
-	$(".fancybox").fancybox({
-		openEffect	: 'none',
-		closeEffect	: 'none'
-	});
-    $('.fancybox-media').fancybox({
-        openEffect  : 'none',
-        closeEffect : 'none',
-        helpers : {
-            media : {}
-        }
-    });
+jQuery(document).ready(function(){
+    $('#tel').mask('+ 7 (000) 000-0000', {placeholder: "Телефон"});
 });
 
 $('.owl-carousel').owlCarousel({
