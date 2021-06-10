cfc95996d1323cd29084f704a131bdb6cdf6ff53
oleksandrbazentka <oleksandrhatake@gmail.com>
2021-06-10 21:44:20 +0300

    done

---
+++ remont-technic/js/main.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+  $('.responsive').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+}
+
+jQuery(document).ready(function(){
+    $('#tel').mask('+7(000) 000 00 00', {placeholder: "+7(___) ___-__-__"});
+});
