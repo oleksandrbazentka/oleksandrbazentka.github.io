6e73aba79b1d10439e05506e63a87f5cf3d3a885
oleksandrbazentka <oleksandrhatake@gmail.com>
2021-11-09 23:56:27 +0200

    done

---
+++ RealTyka/js/main.js
@@ -0,0 +1,49 @@
+window.addEventListener('load', function() {
+	//TABS
+	$('.panel-heading').click(function () {
+		$(this).toggleClass('in').next().slideToggle();
+		$('.panel-heading').not(this).removeClass('in').next().slideUp();
+	});	
+
+	//SLIDER
+	$('.responsive').slick({
+	  dots: true,
+      arrows: true,
+      appendDots: $(".slide-m-dots"),
+      prevArrow: $(".slide-m-prev"),
+      nextArrow: $(".slide-m-next"),
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 3,
+	  slidesToScroll: 3,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        infinite: true,
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
+	});
+
+
+});
