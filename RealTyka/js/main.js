94a9eae0e3c34f1f7e05e07f6241bf90fb79ccd3
oleksandrbazentka <oleksandrhatake@gmail.com>
2021-11-11 00:47:57 +0200

    done

--- RealTyka/js/main.js
+++ RealTyka/js/main.js
@@ -26,17 +26,18 @@ window.addEventListener('load', function() {
 	      }
 	    },
 	    {
-	      breakpoint: 600,
+	      breakpoint: 768,
 	      settings: {
 	        slidesToShow: 2,
-	        slidesToScroll: 2
+	        slidesToScroll: 2,
 	      }
 	    },
 	    {
 	      breakpoint: 480,
 	      settings: {
 	        slidesToShow: 1,
-	        slidesToScroll: 1
+	        slidesToScroll: 1,
+	        dots: false
 	      }
 	    }
 	    // You can unslick at a given breakpoint now by adding:
@@ -45,5 +46,44 @@ window.addEventListener('load', function() {
 	  ]
 	});
 
+	// ПРОВЕРКА ОБЪЕКТА
+	let checkObjectBtn = document.querySelector('.first__submit');
+	checkObjectBtn.addEventListener('click', function(e) {
+		e.preventDefault();
+		let preload = document.querySelector('.found__preload-block');
+		let objects = document.querySelector('.found__block');
+		preload.classList.add('preload-active');
 
+		if(!(objects.classList.contains('found-active'))) {
+			let b = new Promise(function( resolve, reject ) {
+				setTimeout(function(){
+					resolve(preload.classList.remove('preload-active'));
+				}, 4000);
+			});
+			b.then(function() {
+				objects.classList.add('found-active');
+			});
+		} else {
+			objects.classList.remove('found-active');
+			let b = new Promise(function( resolve, reject ) {
+				setTimeout(function(){
+					resolve(preload.classList.remove('preload-active'));
+				}, 4000);
+			});
+			b.then(function() {
+				objects.classList.add('found-active');
+			});
+		}
+	});
+
+	//MENU-GAMBURGER
+	const hamburger = document.querySelector(".hamburger");
+	const navMenu = document.querySelector(".nav-menu");
+	const html = document.querySelector('html');
+	hamburger.addEventListener("click", mobileMenu);
+	function mobileMenu() {
+	    hamburger.classList.toggle("active");
+	    navMenu.classList.toggle("active");
+	    html.classList.toggle('overflow');
+	};
 });
